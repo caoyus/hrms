21ba5c03a640dceb92adae9a5aafa41439593074
caoyus <caoyusong9527@163.com>
2021-05-04 12:46:30 +0800

    init code

---
+++ src/setting.js
@@ -0,0 +1,20 @@
+// 这是一个js文件, 导出了一个对象
+module.exports = {
+
+  // 网站标题
+  title: '人力资源管理系统-78期',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 控制头部是否固定定位 (有些配置数据 是存vuex中, 持久化到本地)
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 控制侧边栏组件的Logo的展示 vuex存储
+  sidebarLogo: true
+}
